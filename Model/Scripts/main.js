cd6c7f038b17edbf32e3b3aaf74dc53ce9ef9b64
Luis Fernández <luiggi@fernandez.org.mx>
2019-10-18 18:19:24 -0500

    NUMUS

--- Model/Scripts/main.js
+++ Model/Scripts/main.js
@@ -1,1 +1,25 @@
+window.onload = function() {
+  checkAuthStatus();
 
+  });
+}
+
+function checkAuthStatus() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      if (userID == "") {
+        userID = user.uid;
+      }
+      getCompanyData(function(){
+        //callback
+      });
+    }
+}
+
+function getCompanyData(callback) {
+  var ref = firebase.database().ref("companies/").child(user.uid);
+    ref.once("value", function(snapshot){
+      document.getElementById('nav-bar-company').innerHTML = snapshot.val().companyName;
+      callback();
+    });
+}
