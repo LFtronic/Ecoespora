d959443558f3447ba24df6d96420e85c000420c0
Luis Fernández <luiggi@fernandez.org.mx>
2019-10-18 14:14:03 -0500

    whaaaaaaa

---
+++ Model/Scripts/db_init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+   apiKey: "AIzaSyBjS2clxv7iWyaVD6yo218H4O_iBaA80RM",
+   authDomain: "ecosporas-hbdcm1.firebaseapp.com",
+   databaseURL: "https://ecosporas-hbdcm1.firebaseio.com",
+   projectId: "ecosporas-hbdcm1",
+   storageBucket: "ecosporas-hbdcm1.appspot.com",
+   messagingSenderId: "888386380731",
+   appId: "1:888386380731:web:008e02b836de2f772cda9e"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
