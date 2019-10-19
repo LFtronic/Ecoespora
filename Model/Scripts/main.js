579f463155b57caee2e3ce45b80b1d527f538fb8
Luis Fernández <luiggi@fernandez.org.mx>
2019-10-19 08:49:27 -0500

    lots of changes

--- Model/Scripts/main.js
+++ Model/Scripts/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   checkAuthStatus();
+  getRankingData();
 }
 
 function checkAuthStatus() {
@@ -21,4 +22,28 @@ function getCompanyData(callback) {
       document.getElementById('nav-bar-company').innerHTML = snapshot.val().companyName;
       callback();
     });
+}
+
+function getRankingData() {
+  var ref = firebase.database().ref("companies/");
+  ref.once("value", function(snapshot){
+    try {
+      var ranking = document.getElementById('main_ranking');
+      snapshot.forEach(function(childSnapshot) {
+        var newTr = document.createElement('tr').className = "main-ranking-row";
+        var newTd1 = document.createElement('td').className = "main-ranking-body-cell";
+        newTd1.innerHTML = "0";
+        var newTd2 = document.createElement('td').className = "main-ranking-body-cell";
+        newTd2.innerHTML = childSnapshot.val().companyName;
+        var newTd3 = document.createElement('td').className = "main-ranking-body-cell";
+        newTd3.innerHTML = childSnapshot.val().rankingPoints;
+        newTr.appendChild(newTd1);
+        newTr.appendChild(newTd2);
+        newTr.appendChild(newTd3);
+        ranking.appendChild(newTr);
+      });
+    } catch {
+
+    }
+  });
 }
